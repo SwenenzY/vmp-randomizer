a720334e72d75f8f9ab633af9e82d101454d1a42
SwenenzY <mail@swenenzy.com>
2021-02-23 10:12:05 +0300

    Final

--- main.cpp
+++ main.cpp
@@ -4,45 +4,120 @@ std::filesystem::directory_entry vmpPath;
 
 int main(int argc, char* argv[])
 {
-   
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    bool isObfuscated = false;
+    // main
     SetConsoleTitleA("VMP-Randomizer by SwenenzY :)");
     std::cout << "[+] Initializing..." << std::endl;
 
-    if (argv[1]) {
-        if (std::filesystem::exists(argv[1])) {
-            std::cout << "[!] Working directory : " << argv[1] << std::endl;
+    // have args 
+    if (argv[1]) { // vmp-randomizer.exe vmp-randomizer.exe.vmp
+        if (std::filesystem::exists(argv[1])) { // is file exists ?
+            std::cout << "[!] Working directory : " << argv[1] << std::endl; // print
+            std::cout << "[+] Starting obfuscation." << std::endl; // print
+            // obfuscate here
+            isObfuscated = ObfuscateVMP(argv[1]);
+            if(isObfuscated)  std::cout << "[+] Successfly obfuscated." << std::endl;
         }
-        else {
-            std::cout << "[-] The specified path does not contain vmp." << std::endl;
+        else { // file doesnt exists
+            std::cout << "[-] The specified path does not contain vmp." << std::endl; // print
         }
     }
-    else {
-        std::cout << "[-] Vmp not specified. Trying to find automatically" << std::endl;
+    else { // vmp not specified we need to found auto
+        std::cout << "[-] Vmp not specified. Trying to find automatically" << std::endl; // print
 
-        string dir = std::filesystem::current_path().string() + "\\";
-        std::cout << "[!] Working directory : " << dir << std::endl;
+        // current dir + "\\" "C:\Users\Administrator\source\repos\vmp-randomizer\x64\"
+        string dir = std::filesystem::current_path().string() + "\\"; 
+        std::cout << "[!] Working directory : " << dir << std::endl; // print
 
-        bool Status = false;
+        bool Status = false; // for check
         for (const auto& entry : fs::directory_iterator(std::filesystem::current_path()))
         {
+            // if file extension is vmp
             if (fs::path(entry.path()).extension().string() == ".vmp") {
+                // save status true and entry
                 Status = true;
                 vmpPath = entry;
             }
         }
+        // if vmp
         if (Status) {
+            // first check file for bug check
             if (std::filesystem::exists(vmpPath.path().string())) {
-                std::cout << "[+]" << std::endl;
+                std::cout << "[+] Starting obfuscation." << std::endl; //print
+                // obfuscate here
+                isObfuscated = ObfuscateVMP(vmpPath.path().string());
+                if (isObfuscated)  std::cout << "[+] Successfly obfuscated." << std::endl;
             }
+            // error
             else {
-                std::cout << "[-] Failed to get vmp path." << std::endl;
+                std::cout << "[-] Failed to get vmp path." << std::endl; //print
             }
         }
+        // error
         else {
-            std::cout << "[-] System failed or folder does not contain vmp." << std::endl;
+            std::cout << "[-] System failed or folder does not contain vmp." << std::endl; //print
         }
     }
 
-    system("pause");
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    if(isObfuscated) std::cout << "\n[-] Finished operation in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << std::endl;
     return 0;
+}
+
+bool ObfuscateVMP(string path) {
+    bool Status = false;
+    // first check why its 3.? I don't know too just beautiful: D
+    if (std::filesystem::exists(path)) {
+        // we need to found .custom
+        // <Protection InputFileName="vmp-randomizer.exe" Options="857032" VMCodeSectionName=".Custom">
+        // VMCodeSectionName=".Custom"
+
+        std::ifstream file(path);
+        std::string str;
+        std::string tempfile;
+        while (std::getline(file, str))
+        {
+            // if line have VMCodeSectionName 
+            if (str.find("VMCodeSectionName") != std::string::npos) {
+                // trim to VMCodeSectionName
+                std::string SectionString = str.substr(str.find("VMCodeSectionName")); // VMCodeSectionName=".Custom">
+                // trim VMCodeSectionName
+                replaceAll(SectionString,"VMCodeSectionName=",""); //".Custom">
+                // trim junk
+                replaceAll(SectionString, "\"", ""); // .Custom>
+                // trim junk
+                replaceAll(SectionString, ">", ""); // .Custom
+                // print 
+                std::cout << "[!] Old Section : << " << SectionString << std::endl;
+
+                //setup random
+                srand((unsigned int)time(NULL));
+                int RandomHash = rand() % 50;
+                // if random number lower 4 rundem number 16
+                if (RandomHash < 4) RandomHash = 16;
+                // create string from random number
+                string RandomSection = "." + random_string_only_char(RandomHash);
+                // print
+                std::cout << "[+] New Section : << " << RandomSection << std::endl;
+                // replace old section to new section
+                replaceAll(str, SectionString, RandomSection);
+                // save status
+                Status = true;
+            }
+            // save temp file
+            tempfile += str + "\n";
+        }
+        // only re write when found
+        if (Status) {
+            // create ofstream
+            ofstream myfile;
+            // open stream
+            myfile.open(path);
+            myfile << tempfile; // re output file
+            // close stream
+            myfile.close();
+        }
+    }
+    return Status;
 }
--- stdafx.h
+++ stdafx.h
@@ -1,14 +1,41 @@
 #include <Windows.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <filesystem>
+#include <random>
 
 // Namespace
 using namespace std;
 namespace fs = std::filesystem;
 
+bool ObfuscateVMP(string path);
 
 std::string GetCurrentPath()
 {
 	char result[MAX_PATH];
 	return std::string(result, GetModuleFileNameA(NULL, result, MAX_PATH));
+}
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if (from.empty())
+        return;
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+    }
+}
+
+std::string random_string_only_char(std::string::size_type length)
+{
+    string possible_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    random_device rd;
+    mt19937 engine(rd());
+    uniform_int_distribution<> dist(0, possible_characters.size() - 1);
+    string ret = "";
+    for (int i = 0; i < length; i++) {
+        int random_index = dist(engine); //get index between 0 and possible_characters.size()-1
+        ret += possible_characters[random_index];
+    }
+    return ret;
 }
