f253a6f9b3876473fda4ee12f8cbe05fbb177646
SwenenzY <mail@swenenzy.com>
2021-02-23 09:31:58 +0300

    Finding VMP

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "stdafx.h"
 
+std::filesystem::directory_entry vmpPath;
+
 int main(int argc, char* argv[])
 {
+   
     SetConsoleTitleA("VMP-Randomizer by SwenenzY :)");
     std::cout << "[+] Initializing..." << std::endl;
 
@@ -14,10 +17,34 @@ int main(int argc, char* argv[])
         }
     }
     else {
+        std::cout << "[-] Vmp not specified. Trying to find automatically" << std::endl;
+
+        string dir = std::filesystem::current_path().string() + "\\";
+        std::cout << "[!] Working directory : " << dir << std::endl;
+
+        bool Status = false;
         for (const auto& entry : fs::directory_iterator(std::filesystem::current_path()))
         {
-            std::cout << entry.path().string() << std::endl;
+            string ext = fs::path(entry.path()).extension().string();
+            std::cout << ext << std::endl;
+            if (ext == ".vmp") { 
+                Status = true;
+                vmpPath = entry;
+            }
         }
-        std::cout << "where is the vmp amk cocu.";
+        if (Status) {
+            if (std::filesystem::exists(vmpPath.path().string())) {
+                std::cout << "[+]" << std::endl;
+            }
+            else {
+                std::cout << "[-] Failed to get vmp path." << std::endl;
+            }
+        }
+        else {
+            std::cout << "[-] System failed or folder does not contain vmp." << std::endl;
+        }
     }
+
+    system("pause");
+    return 0;
 }
